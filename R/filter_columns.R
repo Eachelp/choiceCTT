e6e2b2bf1ed9b6d7d1fc9d2dfc51c45e75777e98
Eachelp <sangminchoi8487@gmail.com>
2024-04-16 15:39:50 +0900

    commit

---
+++ R/filter_columns.R
@@ -0,0 +1,9 @@
+#'
+#'
+#'
+#' @export
+filter_columns <- function(data, items, variable) {
+  valid_columns <- items[[variable]][items[[variable]] != "" & !is.na(items[[variable]])]
+  data[, valid_columns, drop = FALSE]
+}
+
