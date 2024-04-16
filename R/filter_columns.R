737ca5f3214dd38ff059c8e1004f5457c0500b05
Eachelp <sangminchoi8487@gmail.com>
2024-04-16 15:44:14 +0900

    commit

--- R/filter_columns.R
+++ R/filter_columns.R
@@ -6,4 +6,3 @@ filter_columns <- function(data, items, variable) {
   valid_columns <- items[[variable]][items[[variable]] != "" & !is.na(items[[variable]])]
   data[, valid_columns, drop = FALSE]
 }
-
