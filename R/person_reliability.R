fd5f8c6484de19c874724b553f41bd407042e4c9
Eachelp <sangminchoi8487@gmail.com>
2024-04-17 09:36:44 +0900

    commit

--- R/person_reliability.R
+++ R/person_reliability.R
@@ -1,10 +1,12 @@
 #' person reliability
 #'
 #'
-#' @export
 #'
-person_reliability <- function(data, variance_threshold = 0.1, relMax = NULL) {
-  # Set the default value for relMax if it's not provided
+#' @export
+person_reliability <- function(data, variance_threshold = 0.1, relMax = NULL,
+                               careless_items = NULL, careless_response = c(3,4,5),
+                               careless_MAX = 2) {
+  # Set default value for relMax if not provided
   if (is.null(relMax)) {
     relMax <- ncol(data) / 3  # Default to one-third of the number of columns
   }
@@ -24,15 +26,20 @@ person_reliability <- function(data, variance_threshold = 0.1, relMax = NULL) {
       max(rle(non_na_row)$lengths)  # Calculate maximum consecutive run length
     }),
     count_NA = apply(data, 1, function(row) sum(is.na(row) | row == "" | row == "NA")),
+    Response_Count = if (!is.null(careless_items) && all(careless_items %in% names(data))) {
+      apply(data[, careless_items, drop = FALSE], 1, function(x) sum(x %in% careless_response, na.rm = TRUE))
+    } else rep(NA, nrow(data)),  # Return NA for all rows if careless_items are null or missing
     row.names = row.names(data)
   )
 
   # Mark rows based on variance and run length thresholds
   df$Variance_Mark = ifelse(df$Variance <= variance_threshold, "O", "")
   df$Run_Mark = ifelse(df$Max_Run >= relMax, "O", "")
+  df$careless_Mark = ifelse(df$Response_Count >= careless_MAX, "O", "")
 
   return(df)
 }
+
 
 
 
