5d849e255d2499dd486256dbb4e2fe3aae0e7aef
Eachelp <sangminchoi8487@gmail.com>
2024-04-17 09:18:52 +0900

    commit

--- R/person_reliability.R
+++ R/person_reliability.R
@@ -4,28 +4,35 @@
 #' @export
 #'
 person_reliability <- function(data, variance_threshold = 0.1, relMax = NULL) {
-  # Set default value for relMax if not provided
+  # Set the default value for relMax if it's not provided
   if (is.null(relMax)) {
-    relMax <- ncol(data) / 3  # Default to one third of the number of columns
+    relMax <- ncol(data) / 3  # Default to one-third of the number of columns
   }
 
-  # Calculate variance, maximum consecutive run length, and count NA and empty strings
+  # Convert all data to numeric, coercing errors to NA
+  data_numeric <- data.frame(lapply(data, function(x) as.numeric(as.character(x))))
+
+  # Calculate variance, maximum run length, and count missing values including empty strings and "NA" texts
   df <- data.frame(
-    Variance = apply(data, 1, function(row) var(row, na.rm = TRUE)),  # Calculate variance excluding NA values
-    Max_Run = apply(data, 1, function(row) {
-      non_na_row = row[!is.na(row) & row != ""]  # Remove NA and empty strings before calculating run length
-      if (length(non_na_row) == 0) return(0)  # Return 0 if the row is empty after removing NA and ""
+    Variance = apply(data_numeric, 1, function(row) {
+      if (all(is.na(row))) return(NA)  # Return NA if all values are NA
+      var(row, na.rm = TRUE)  # Calculate variance excluding NAs
+    }),
+    Max_Run = apply(data_numeric, 1, function(row) {
+      non_na_row = row[!is.na(row)]
+      if (length(non_na_row) == 0) return(0)  # Return 0 if the row is empty after removing NAs
       max(rle(non_na_row)$lengths)  # Calculate maximum consecutive run length
     }),
-    count_NA = apply(data, 1, function(row) sum(is.na(row) | row == "")),  # Count the number of NA and empty strings
-    row.names = row.names(data)  # Preserve original row names
+    count_NA = apply(data, 1, function(row) sum(is.na(row) | row == "" | row == "NA")),
+    row.names = row.names(data)
   )
 
   # Mark rows based on variance and run length thresholds
-  df$Variance_Mark = ifelse(df$Variance <= variance_threshold, "O", "")  # Mark "O" if variance is below threshold
-  df$Run_Mark = ifelse(df$Max_Run >= relMax, "O", "")  # Mark "O" if maximum run length is above the threshold
+  df$Variance_Mark = ifelse(df$Variance <= variance_threshold, "O", "")
+  df$Run_Mark = ifelse(df$Max_Run >= relMax, "O", "")
 
   return(df)
 }
+
 
 
